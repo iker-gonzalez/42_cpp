5e22e3c8ce5e20384ae494c7ce9e54941121a30e
Ikgonzal <ikgonzal@aiagasesores.com>
2022-05-13 10:40:59 +0200

    ex04 ++

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ikgonzal <ikgonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 10:42:44 by ikgonzal          #+#    #+#             */
-/*   Updated: 2022/05/12 13:31:17 by ikgonzal         ###   ########.fr       */
+/*   Updated: 2022/05/13 10:39:58 by ikgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,29 @@ int	error(std::string err)
 	return(1);
 }
 
+bool	setFile(std::string file, std::string s1, std::string s2)
+{
+    std::ifstream f(file);
+	std::ofstream outfile;
+	std::string	line;
+
+
+	outfile.open(file, std::ios::out | std::ios::in);
+	while(std::getline(f, line))
+	{
+		std::size_t pos = line.find(s1);
+		if (pos != std::string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+		}
+		std::cout << line << std::endl;
+		outfile.open(file);
+		outfile << line;
+	}
+	return true;
+}
+
 int main (int argc, char **argv)
 {
 	std::fstream	file;
@@ -26,5 +49,6 @@ int main (int argc, char **argv)
 	
 	if (argc != 4)
 		return(error("Wrong number of arguments"));
-	file.open(argv[1]);
+	if (!setFile(argv[1], argv[2], argv[3]))
+		return(error("Could not set the file"));
 }
