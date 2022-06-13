b9d29c5ad0e24cda7a135157f459fffd97f7aa5f
iker-gonzalez <ikgonzal@aiagasesores.com>
2022-06-13 18:56:20 +0200

    ex00 finished

--- cpp_07/ex00/main.cpp
+++ cpp_07/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikgonzal <ikgonzal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ikgonzal <ikgonzal@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 10:54:34 by ikgonzal          #+#    #+#             */
-/*   Updated: 2022/06/13 12:45:27 by ikgonzal         ###   ########.fr       */
+/*   Updated: 2022/06/13 18:55:48 by ikgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,18 @@
 
 int main (void) {
 
-	int a = 5;
-	int b = 42;
-	
-	swap<int>(a, b);
-	
-	char c = 'c';
-	char d = 'd';
-	swap<char>(c, d);
-	
-	int e = 9;
-	int f = 10;
-	int minimum;
-	
-	minimum = min<int>(e, f);
-	std::cout << minimum << std::endl;
-	std::cout << "--------" << std::endl;
-	
-	double g = 5.744f;
-	double i = 6.25f;
-	
-	std::cout << g << std::endl;
-	std::cout << min<float>(g, i) << std::endl;
-	//std::cout << minimum << std::setprecision(2) << std::endl;
-	std::cout << "--------" << std::endl;
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	
 }
--- cpp_07/ex00/template.hpp
+++ cpp_07/ex00/template.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   template.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikgonzal <ikgonzal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ikgonzal <ikgonzal@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 10:59:18 by ikgonzal          #+#    #+#             */
-/*   Updated: 2022/06/13 11:28:17 by ikgonzal         ###   ########.fr       */
+/*   Updated: 2022/06/13 18:55:41 by ikgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,27 @@
 #include <iomanip>
 
 template <typename T>
-void swap(T var1, T var2) {
+void swap(T &var1, T &var2) {
 
 	T var3;
 	
 	var3 = var1;
 	var1 = var2;
 	var2 = var3;
-	std::cout << "var1: " << var1 << std::endl;
-	std::cout << "var2: " << var2 << std::endl;
-	std::cout << "--------------" << std::endl;
 }
 
 template <typename T>
 T min(T var1, T var2) {
 	
 	if (var1 >= var2)
+		return var2;
+	return var1;
+}
+
+template <typename T>
+T max(T var1, T var2) {
+	
+	if (var1 <= var2)
 		return var2;
 	return var1;
 }
