2439521e72a38939755dfd46ee08c870e8fccf67
iker-gonzalez <ikgonzal@aiagasesores.com>
2022-06-24 09:13:08 +0200

    ex00 done

--- cpp_08/main.cpp
+++ cpp_08/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ikgonzal <ikgonzal@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 10:56:48 by ikgonzal          #+#    #+#             */
-/*   Updated: 2022/06/23 12:25:59 by ikgonzal         ###   ########.fr       */
+/*   Updated: 2022/06/24 09:12:42 by ikgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,81 +14,63 @@
 #include <iostream>
 
 #include <vector>
-#include <stack>
-#include <queue>
+#include <deque>
+#include <list>
 
 void    printVals(int val) {
-    std::cout << val << ", ";
+	std::cout << val << ", ";
 }
 
 template <typename T>
-void    testVector(T &cont, int &val) {
+void    test(T &cont, int &val) {
 
-    try {
-        std::cout << "Find => " << val << " in the following container" << std::endl;
-        for_each(cont.begin(), cont.end(), printVals);
-        std::cout << std::endl;
-        easyfind(cont, val);
-        std::cout << " ✅ The container has the value " << val << std::endl;
-    }
-    catch(std::exception) {
-        std::cout << "❌ There is no coincidence" << std::endl;
-    }
-    std::cout << std::endl;
+	try {
+		std::cout << "Find => " << val << " in the following container" << std::endl;
+		for_each(cont.begin(), cont.end(), printVals);
+		std::cout << std::endl;
+		easyfind(cont, val);
+		std::cout << " ✅ The container has the value " << val << std::endl;
+	}
+	catch(std::exception) {
+		std::cout << "❌ There is no coincidence" << std::endl;
+	}
+	std::cout << std::endl;
 }
-
-template <typename T>
-void    testStack(T &cont, int &val) {
-
-    try {
-        std::cout << "Find => " << val << " in the following container" << std::endl;
-        for_each(cont.front(), cont.back(), printVals);
-        std::cout << std::endl;
-        easyfind(cont, val);
-        std::cout << " ✅ The container has the value " << val << std::endl;
-    }
-    catch(std::exception) {
-        std::cout << "❌ There is no coincidence" << std::endl;
-    }
-    std::cout << std::endl;
-}
-
-
 
 int main(void){
 
-    std::vector<int> vec;;
-    std::stack<int> st;;
-    std::queue<int> qu;
+	std::vector<int> vec;;
+	std::deque<int> deq;;
+	std::list<int> li;
 
-    //values 
-    int a = 3, b = 42, c = 8;
+	//values 
+	int a = 3, b = 42, c = 8;
 
-    //determinants
-    int success = 42;
-    int fail = 1;
+	//determinants
+	int success = 42;
+	int fail = 1;
 
-    //vector
-    vec.push_back(a);
-    vec.push_back(b);
-    vec.push_back(c);
+	//vector
+	vec.push_back(a);
+	vec.push_back(b);
+	vec.push_back(c);
 
-    //stack
-    st.push(a);
-    st.push(b);
-    st.push(c);
+	//deque
+	deq.push_back(a);
+	deq.push_back(b);
+	deq.push_back(c);
 
-    //queue
-    qu.push(a);
-    qu.push(b);
-    qu.push(c);
+	//list
+	li.push_front(a);
+	li.push_front(b);
+	li.push_front(c);
 
-    testVector(vec, success);
-    testVector(vec, fail);
-    testStack(st, success);
-    testStack(st, fail);
-    //test(qu, success);
-    //test(qu, fail);
-    
-    return 0;
+	test(vec, success);
+	test(vec, fail);
+	test(deq, success);
+	test(deq, fail);
+	test(li, success);
+	test(li, fail);
+
+	return 0;
 }
